d3d8bc1901d8bbfd176b0d4e7bfaec615fe65946
James Barr <barr.james.r@gmail.com>
2018-08-24 18:24:07 -0700

    Simplify enum and use data classes

--- src/main/kotlin/com/commit451/resourcespoet/Plural.kt
+++ src/main/kotlin/com/commit451/resourcespoet/Plural.kt
@@ -4,13 +4,13 @@ package com.commit451.resourcespoet
  * Represents an Android Plural
  * See <a https:></a>//developer.android.com/guide/topics/resources/string-resource.html#Plurals">https://developer.android.com/guide/topics/resources/string-resource.html#Plurals
  */
-class Plural
+data class Plural
 /**
  * Construct a new plural
  * @param quantity the quantity
  * @param value the value
  */
-(var quantity: Quantity, var value: String) {
+(val quantity: Quantity, val value: String) {
 
     enum class Quantity {
         zero,
