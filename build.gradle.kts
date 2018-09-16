a39f67af3c82005a15e2dddb20c400f65fb12ddb
John Carlson <jawnnypoo@gmail.com>
2018-09-15 20:50:28 -0700

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,10 @@ import org.gradle.jvm.tasks.Jar
 plugins {
     `build-scan`
     `maven-publish`
-    kotlin("jvm") version "1.2.61"
-    id("org.jetbrains.dokka") version "0.9.16"
+    kotlin("jvm") version "1.2.70"
+    id("org.jetbrains.dokka") version "0.9.17"
     id("com.jfrog.bintray") version "1.8.4"
+    id("com.github.ben-manes.versions") version "0.20.0"
 }
 
 group = "com.commit451"
@@ -18,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib", "1.2.61"))
+    implementation(kotlin("stdlib", "1.2.70"))
     testImplementation("junit:junit:4.12")
     testImplementation("com.google.guava:guava:26.0-jre")
 }
