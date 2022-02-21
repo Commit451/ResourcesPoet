e9cef238e85a2ee12cdd2100e002c815707d5ff2
John Carlson <jawnnypoo@gmail.com>
2022-02-21 03:22:35 -0600

    Fix link, remove constructor docs that are not viewable

--- src/main/kotlin/com/commit451/resourcespoet/FontFamily.kt
+++ src/main/kotlin/com/commit451/resourcespoet/FontFamily.kt
@@ -2,13 +2,10 @@ package com.commit451.resourcespoet
 
 /**
  * Represents an Android Font
- * See <a https:></a>//developer.android.com/guide/topics/ui/look-and-feel/fonts-in-xml">https://developer.android.com/guide/topics/ui/look-and-feel/fonts-in-xml
+ * See [the Android docs](https://developer.android.com/guide/topics/ui/look-and-feel/fonts-in-xml)
  */
-data class FontFamily
-/**
- * Construct a Font family
- * @param fontStyle the font style
- * @param fontWeight the font weight value
- * @param font reference to the font file
- */
-    (val fontStyle: String, val fontWeight: String, val font: String)
+data class FontFamily(
+    val fontStyle: String,
+    val fontWeight: String,
+    val font: String
+)
